ecf8a579dd378584ed138cc71efa37dc39a02fd7
R. Jordan Crouser <jcrouser@smith.edu>
2019-06-23 22:13:36 -0400

    2019 updates

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/jcrouser/Google Drive/Synergistic Activities/MassMutual/2018/MassMutual-DataVis")
+setwd("/Users/jcrouser/Google Drive/Synergistic Activities/MassMutual/2019/MassMutual-DataVis")
 
 #render site. 
 rmarkdown::render_site()
